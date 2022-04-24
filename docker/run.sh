0207093896d7917cfa13af205fbf96523782745a
intuinno <intuinno@gmail.com>
2022-04-24 08:30:52 +0000

    Added a prompt

--- docker/run.sh
+++ docker/run.sh
@@ -6,11 +6,31 @@ echo "example usage run.sh device name xserver"
 if [ $# -eq 2 ]
 then
     echo "running docker without display"
-    docker run -it --rm -v ~/.docker_bash_history:/home/intuinno/.bash_history -v /home/intuinno/codegit/:/home/intuinno/codegit:Z -v /home/intuinno/.ssh/:/home/intuinno/.ssh -w /home/intuinno --user "$(id -u):$(id -g)" --ipc=host --network=host --gpus $1   --name=$2 skinner_legged /bin/bash
+    docker run -it --rm \
+	-v /home/intuinno/codegit/:/home/intuinno/codegit \
+	-v /home/intuinno/.ssh/:/home/intuinno/.ssh \
+        -v /home/intuinno/.oh-my-zsh/:/home/intuinno/.oh-my-zsh \
+	-v "$HOME/.docker_zsh_history:/home/intuinno/.zsh_history:rw" \
+        -v "$HOME/.zshrc:/home/intuinno/.zshrc:ro" \
+        -e DOCKER_MACHINE_NAME=$2 \
+       --user "$(id -u):$(id -g)" \
+       --ipc=host --network=host --gpus $1 --name=$2 \
+       skinner_legged /bin/zsh
 else
     export DISPLAY=$DISPLAY
 	echo "setting display to $DISPLAY"
 	xhost +
-	docker run -it --rm -v "$HOME/.Xauthority:/home/intuinno/.Xauthority:rw" -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/intuinno/codegit/:/home/intuinno/codegit:Z -v /home/intuinno/.ssh/:/home/intuinno/.ssh -w /home/intuinno/codegit --ipc=host -e DISPLAY=$DISPLAY --user "$(id -u):$(id -g)" --network=host --gpus $1 --name=$2 -v ~/.docker_bash_history:/home/intuinno/.bash_history skinner_legged /bin/bash 
+	docker run -it --rm \
+	    -v "$HOME/.Xauthority:/home/intuinno/.Xauthority:rw" \
+	    -v /tmp/.X11-unix:/tmp/.X11-unix \
+	    -v /home/intuinno/codegit/:/home/intuinno/codegit \
+	    -v /home/intuinno/.ssh/:/home/intuinno/.ssh \
+	    -v /home/intuinno/.oh-my-zsh/:/home/intuinno/.oh-my-zsh \
+	    -v "$HOME/.docker_zsh_history:/home/intuinno/.zsh_history:rw" \
+            -v "$HOME/.zshrc:/home/intuinno/.zshrc:ro" \
+	    -e PS1="🐳 \e[0;34m$2\e[0m \w # " "]]"
+	    --ipc=host -e DISPLAY=$DISPLAY \
+	    --user "$(id -u):$(id -g)" --network=host --gpus $1 --name=$2 \
+	    skinner_legged /bin/zsh 
 	xhost -
 fi
