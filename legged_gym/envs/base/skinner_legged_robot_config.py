cb49b6732a8e38db205edb056cc0878b56f8db56
intuinno <intuinno@gmail.com>
2022-04-27 13:41:21 -0500

    Increased training time

--- legged_gym/envs/base/skinner_legged_robot_config.py
+++ legged_gym/envs/base/skinner_legged_robot_config.py
@@ -48,7 +48,7 @@ class SkinnerLeggedRobotCfg(BaseConfig):
         num_actions = 12
         env_spacing = 3.  # not used with heightfields/trimeshes 
         send_timeouts = True # send time out information to the algorithm
-        episode_length_s = 20 # episode length in seconds
+        episode_length_s = 2000 # episode length in seconds
 
     class terrain:
         mesh_type = 'trimesh' # "heightfield" # none, plane, heightfield or trimesh
